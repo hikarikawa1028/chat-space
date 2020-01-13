73533661090f852e5ecc1cdc65f9adcda8285258
hikarikawa1028 <okmc7335@yahoo.co.jp>
2020-01-13 20:30:27 +0900

    comi

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,15 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :index
+    end
+  end
+
+
 end
